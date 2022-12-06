066e8eef7a40bd956175d541c8b8164be7dc3c2d
小火娃 <1178844902@qq.com>
2022-12-06 19:06:09 +0800

    修改闪屏页gif

--- app/src/main/java/com/xiaohuowa/lh138/activity/SplashActivity.java
+++ app/src/main/java/com/xiaohuowa/lh138/activity/SplashActivity.java
@@ -23,7 +23,8 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         ImageView imageView = findViewById(R.id.imageView);
         Glide.with(this)
-                .load("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png")
+                .load("https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fsyimg.3dmgame.com%2Fuploadimg%2Fupload%2Fimage%2F20200722%2F20200722141720_84005.gif&refer=http%3A%2F%2Fsyimg.3dmgame.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1672897013&t=e85995b0f5fdc206a8a879b224522c72")
+//                .load("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png")
 //                .load("http://5b0988e595225.cdn.sohucs.com/images/20190831/05de49d16e374e9e997bc97fdf29b0cc.gif")
                 .into(imageView);
         new Handler().postDelayed(() -> {
